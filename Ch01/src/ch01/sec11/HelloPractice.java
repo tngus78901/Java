cd8ff390deaf7818d651b96be7c9e75fa7a62037
tngus78901 <tngus78901@gmail.com>
2023-06-13 15:21:58 +0900

    Ch01 수업예제

---
+++ Ch01/src/ch01/sec11/HelloPractice.java
@@ -0,0 +1,26 @@
+package ch01.sec11;
+/**
+ * 날짜 : 2023.06.12
+ * 이름 : 김수현
+ * 내용 : 29page예제 연습
+ */
+
+
+
+public class HelloPractice {
+   //프로그램 실행 진입점
+	public static void main(String[] args) {
+		//콘솔에 입력하는 실행문
+		System.out.println("Hello,Java");
+		System.out.println("Hello,welcome to the java world!");
+		
+		
+		
+		int x;   //변수 x선언
+		x = 1;   //변수 X에 1값을 저장
+		int y = 2;  //변수 y를 선언하고 2값을 저장
+		int result = x + y;  //변수 result를 선언하고 변수 x와 y를 더한 값을 저장
+		System.out.println(result); //콘솔에 변수의 값을 출력하는 println() 메소드 호출
+	}
+
+}
