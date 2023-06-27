0628f3ece87169934a224d657722e2e9c694d18a
tngus78901 <136422035+tngus78901@users.noreply.github.com>
2023-06-27 17:31:28 +0900

    test

---
+++ CodingTest/src/step1/Test03.java
@@ -0,0 +1,28 @@
+package step1;
+
+public class Test03 {
+
+	
+	public static void main(String[] args) {
+		
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+	}
+}
+
